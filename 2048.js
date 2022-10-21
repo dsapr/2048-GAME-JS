77c70a15ce9c5573809d154d62502071a677f25f
dsapr <785722840@qq.com>
2022-10-22 00:01:46 +0800

    文字居中，block 间距，shift 函数

--- 2048.js
+++ 2048.js
@@ -1,8 +1,11 @@
 // Constant
 CANVAS_SIZE = 600;
-CANVAS_BACKGROUND_COLOR = "333333"
 GAME_SIZE = 4;
-BLOCK_SIZE = 150;
+BLOCK_SIZE = 130;
+GRID_SIZE = CANVAS_SIZE / GAME_SIZE;
+PADDING_SIZE = (CANVAS_SIZE - GAME_SIZE * BLOCK_SIZE) / 5;
+
+CANVAS_BACKGROUND_COLOR = "333333"
 BLOCK_PLACEHOLDER_COLOR = "555555";
 BLOCK_BACKGROUD_COLOR = "664455";
 
@@ -46,7 +49,68 @@ class Game {
         }
         let position = randChoice(possiblePositions);
         this.data[position[0]][position[1]] = 2;
-    }
+	}
+	
+	shiftBlock (arr) {
+		let head = 0;
+		let tail = 1;
+
+		while (tail < arr.length) {
+			if (arr[tail] == null) {
+				tail += 1;
+			} else {
+				if (arr[head] == null) {
+					arr[head] = arr[tail];
+					arr[tail] = null;
+					tail += 1;
+				} else if (arr[head] == arr[tail]) {
+					arr[head] = arr[head] * 2;
+					arr[tail] = null;
+					head += 1;
+					tail += 1;
+				} else {
+					head += 1;
+				}
+			}
+		}
+	}
+}
+
+// Tests
+class Test {
+	static compareArray (arr1, arr2) {
+		if (arr1.length != arr2.length) return false;
+
+		for (let i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+	static test_shiftBlock () {
+		let gameTest = new Game();
+		let testCases = [
+			[[2, 2, 2, 2], [4, 4, null, null]],
+			[[2, 2, null, 2], [4, 2, null, null]]
+		]
+		let errFlag = false;
+
+		for (let test of testCases) {
+			let input = test[0];
+			let result = test[1];
+			gameTest.shiftBlock(input);
+			if (!Test.compareArray(input, result)) {
+				errFlag = true;
+				console.log("Error!")
+			}
+		}
+
+		if (!errFlag) {
+			console.log("Pass!")
+		}
+	}
 }
 
 // View
@@ -62,8 +126,16 @@ class View {
         this.container.style.height = CANVAS_SIZE;
         this.container.style.backgroundColor = CANVAS_BACKGROUND_COLOR;
         this.container.style.position = "relative";
-        this.container.style.display = "inline-block";
-    }
+		this.container.style.display = "inline-block";
+		this.container.style.fontSize = 60;
+	}
+	
+	gridToPosition (i, j) {
+		let top = i * (BLOCK_SIZE + PADDING_SIZE) + PADDING_SIZE;
+		let left = j * (BLOCK_SIZE + PADDING_SIZE) + PADDING_SIZE;
+
+		return [top, left];
+	}
 
     drawGame() {
         for (let i = 0; i < GAME_SIZE; i++) {
@@ -77,13 +149,15 @@ class View {
     }
 
     drawBackgroudBlock(i, j, color) {
-        let block = document.createElement("div");
+		let block = document.createElement("div");
+		let position = this.gridToPosition(i, j);
+
         block.style.width = BLOCK_SIZE;
         block.style.height = BLOCK_SIZE;
         block.style.backgroundColor = color;
         block.style.position = "absolute";
-        block.style.top = i * BLOCK_SIZE;
-        block.style.left = j * BLOCK_SIZE;
+        block.style.top = position[0];
+        block.style.left = position[1];
         this.container.append(block);
         return block;
     }
@@ -94,6 +168,10 @@ class View {
         let block = this.drawBackgroudBlock(i, j, BLOCK_BACKGROUD_COLOR);
         span.appendChild(text);
         block.appendChild(span);
+        // 文字
+		span.style.position = "absolute";
+		span.style.top = (BLOCK_SIZE - span.offsetHeight) / 2;
+		span.style.left = (BLOCK_SIZE - span.offsetWidth) / 2;
     }
 }
 
